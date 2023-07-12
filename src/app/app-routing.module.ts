5368a450f6dfefc9d8b8b618e3927832e4cfad10
aldusd <aldusdanielfsm@gmail.com>
2023-07-12 17:49:02 -0300

    feat: inital layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InitialPageComponent } from './components/pages/initial-page/initial-page.component';
+import { ApplicationFormComponent } from './pages/application-form/application-form.component';
+import { ResultsComponent } from './pages/results/results.component';
+import { ViewComponent } from './pages/view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch:'full', component: InitialPageComponent },
+  { path: 'application/new', component: ApplicationFormComponent, children: [
+    { path: 'results', component: ResultsComponent },
+    { path: 'view', component: ViewComponent },
+  ]},
+  { path: '**', redirectTo: ""},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { InitialPageComponent } from './components/pages/initial-page/initial-page.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ApplicationFormComponent } from './pages/application-form/application-form.component';
+import { ResultsComponent } from './pages/results/results.component';
+import { ViewComponent } from './pages/view/view.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InitialPageComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ApplicationFormComponent,
+    ResultsComponent,
+    ViewComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
